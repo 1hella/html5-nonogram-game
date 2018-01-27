1c6ba7e09d11213ef428110acf62230878bbfd9b
swanhell <swanhell@sfu.ca>
2018-01-26 16:59:54 -0800

    add references

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
 const numRows = 20;
 const numCols = numRows;
 const board = new Array(numRows + 1);
-
 const tbody = document.getElementById('game-table-body');
 
-for (let i = 0; i < numRows + 1; i++) {
-    let row = document.createElement('tr');
-
-    for (let j = 0; j < numCols + 1; j++) {
-        let cell = document.createElement('td');
-        cell.setAttribute('data-row', i);
-        cell.setAttribute('data-col', j);
-        let cellClass = i === 0 || j === 0 ? 'info-cell' : 'game-cell';
-        cell.setAttribute('class', cellClass);
-        row.appendChild(cell);
-    }
-
-    tbody.appendChild(row);
-}
-
+makeTable();
 tbody.addEventListener('click', (e) => {
     let cell = e.target;
     let row = cell.dataset.row;
     let col = cell.dataset.col;
-
-    console.log('you clicked cell [' + row + ', ' + col + ']!');
 });
+
+function makeTable() {
+    for (let i = 0; i < numRows + 1; i++) {
+        let row = document.createElement('tr');
+
+        for (let j = 0; j < numCols + 1; j++) {
+            let cell = document.createElement('td');
+            cell.setAttribute('data-row', i);
+            cell.setAttribute('data-col', j);
+            let cellClass = i === 0 || j === 0 ? 'info-cell' : 'game-cell';
+            cell.setAttribute('class', cellClass);
+            row.appendChild(cell);
+        }
+
+        tbody.appendChild(row);
+    }
+}
---
+++ readme.txt
@@ -0,0 +1,5 @@
+References:
+https://blog.mariusschulz.com/2015/12/31/constant-variables-in-javascript-or-when-const-isnt-constant
+https://stackoverflow.com/questions/507138/how-do-i-add-a-class-to-a-given-element
+https://developer.mozilla.org/en-US/docs/Learn/HTML/Howto/Use_data_attributes
+https://developer.mozilla.org/en-US/docs/Web/API/Document_Object_Model/Traversing_an_HTML_table_with_JavaScript_and_DOM_Interfaces
