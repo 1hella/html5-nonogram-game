e99fa730a50312beaa1b449892d4699cc0ed09da
swanhell <swanhell@sfu.ca>
2018-01-27 21:37:23 -0800

    make game and select cells

--- index.js
+++ index.js
@@ -1,25 +1,64 @@
 const numRows = 20;
 const numCols = numRows;
-const board = new Array(numRows + 1);
+const board = new Array(numRows);
+const tableCells = new Array(numRows + 1);
 const tbody = document.getElementById('game-table-body');
+const percentSpots = 65;
 
 makeTable();
+makeGame();
+
 tbody.addEventListener('click', (e) => {
     let cell = e.target;
     let row = cell.dataset.row;
     let col = cell.dataset.col;
+
+    if (row >= 0 && col >= 0) {
+        board[row][col].isChecked = !board[row][col].isChecked;
+        updateTable();
+    }
 });
+
+// updates game cells, not info cells
+function updateTable() {
+    for (let i = 1; i < numRows + 1; i++) {
+        for (let j = 1; j < numCols + 1; j++) {
+            let tableCell = tableCells[i][j];
+            let cell = board[i - 1][j - 1];
+            if (cell.isChecked) {
+                tableCell.classList.add("checked");
+            } else {
+                tableCell.classList.remove("checked");
+            }
+        }
+    }
+}
+
+function makeGame() {
+    for (let i = 0; i < numRows; i++) {
+        board[i] = new Array(numCols);
+        for (let j = 0; j < numCols; j++) {
+            board[i][j] = {
+                isChecked: false,
+                isSpot: Math.random() >= 1 - (percentSpots / 100)
+            }
+        }
+    }
+}
 
 function makeTable() {
     for (let i = 0; i < numRows + 1; i++) {
+        tableCells[i] = new Array(numCols + 1);
         let row = document.createElement('tr');
 
         for (let j = 0; j < numCols + 1; j++) {
             let cell = document.createElement('td');
-            cell.setAttribute('data-row', i);
-            cell.setAttribute('data-col', j);
-            let cellClass = i === 0 || j === 0 ? 'info-cell' : 'game-cell';
+            let isInfoCell = i === 0 || j === 0;
+            cell.setAttribute('data-row', i - 1);
+            cell.setAttribute('data-col', j - 1);
+            let cellClass = isInfoCell ? 'info-cell' : 'game-cell';
             cell.setAttribute('class', cellClass);
+            tableCells[i][j] = cell;
             row.appendChild(cell);
         }
 
--- readme.txt
+++ readme.txt
@@ -3,3 +3,4 @@ https://blog.mariusschulz.com/2015/12/31/constant-variables-in-javascript-or-whe
 https://stackoverflow.com/questions/507138/how-do-i-add-a-class-to-a-given-element
 https://developer.mozilla.org/en-US/docs/Learn/HTML/Howto/Use_data_attributes
 https://developer.mozilla.org/en-US/docs/Web/API/Document_Object_Model/Traversing_an_HTML_table_with_JavaScript_and_DOM_Interfaces
+https://stackoverflow.com/questions/35578478/array-prototype-fill-with-object-passes-reference-and-not-new-instance
